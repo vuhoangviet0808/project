9a38a7c94b270a056565fa2b5c2d8b367d5a9aa5
John Doe <cuonga2242002@gmail.com>
2024-11-24 23:16:24 +0700

    UI-update

--- libs/client_handler.h
+++
@@ -1,7 +0,0 @@
-#ifndef CLIENT_HANDLER_H
-#define CLIENT_HANDLER_H
-
-void *client_handler(void *socket_desc);
-void init_clients();
-
-#endif
--- libs/message_handler.h
+++
@@ -1,8 +0,0 @@
-#ifndef MESSAGE_HANDLER_H
-#define MESSAGE_HANDLER_H
-
-#include "common.h"
-
-void send_private_message(int sender_id, int receiver_id, const char *message);
-
-#endif
