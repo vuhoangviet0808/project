550b6650d878b091741c97f0f71ccf449be20e85
John Doe <cuonga2242002@gmail.com>
2024-12-09 15:08:40 +0700

    12/9

--- libs/user_manager.c
+++ libs/user_manager.c
@@ -1,40 +1,48 @@
 #include "user_manager.h"
 
-
-int load_next_id() {
+int load_next_id()
+{
     int next_id;
     FILE *file = fopen(ID_FILE, "r");
-    if (file) {
+    if (file)
+    {
         fscanf(file, "%d", &next_id);
         fclose(file);
-    } else {
-        next_id = 0;  
+    }
+    else
+    {
+        next_id = 0;
     }
     return next_id;
 }
 
-void save_next_id(int next_id) {
+void save_next_id(int next_id)
+{
     FILE *file = fopen(ID_FILE, "w");
-    if (file) {
+    if (file)
+    {
         fprintf(file, "%d", next_id);
         fclose(file);
     }
 }
 
-int create_user_directory(const char *username, const char *password) {
+int create_user_directory(const char *username, const char *password)
+{
     char user_dir[BUFFER_SIZE];
     snprintf(user_dir, sizeof(user_dir), "%s/%s", BASE_DIR, username);
 
-    if (mkdir(user_dir, 0700) == 0) {
+    if (mkdir(user_dir, 0700) == 0)
+    {
         int user_id = load_next_id();
         int next_id = user_id + 1;
-        save_next_id(next_id);  
+        save_next_id(next_id);
 
         char info_file[BUFFER_SIZE];
         snprintf(info_file, sizeof(info_file), "%s/info.txt", user_dir);
 
         FILE *file = fopen(info_file, "w");
-        if (file) {
+        if (file)
+        {
             fprintf(file, "ID:%d\nPASSWORD:%s\n", user_id, password);
             fclose(file);
         }
@@ -46,15 +54,17 @@ int create_user_directory(const char *username, const char *password) {
     return -1;
 }
 
-
-int load_user_name(Client *clients, int max_clients) {
+int load_user_name(Client *clients, int max_clients)
+{
     int i = 0, user_id;
     FILE *file = fopen(U_FILE, "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         fprintf(stderr, "Error: Unable to open user file.\n");
-        return -1; 
+        return -1;
     }
-    for(int j=0;j<MAX_FRIENDS;j++){
+    for (int j = 0; j < MAX_FRIENDS; j++)
+    {
         clients[j].id = -1;
         clients[j].is_online = 0;
         clients[j].username[0] = '\0';
@@ -63,22 +73,26 @@ int load_user_name(Client *clients, int max_clients) {
         clients[j].friends[i] = -1;
     }
     char buffer[BUFFER_SIZE];
-    while (i < max_clients && fgets(buffer, sizeof(buffer), file) != NULL) {
+    while (i < max_clients && fgets(buffer, sizeof(buffer), file) != NULL)
+    {
         buffer[strcspn(buffer, "\n")] = '\0';
         char info_file[BUFFER_SIZE];
         snprintf(info_file, sizeof(info_file), "%s/%s/info.txt", BASE_DIR, buffer);
         FILE *inf_file = fopen(info_file, "r");
-        if (inf_file == NULL) {
+        if (inf_file == NULL)
+        {
             fprintf(stderr, "Error: Unable to open info file for user '%s'.\n", buffer);
-            continue; 
+            continue;
         }
-        
+
         char buff[BUFFER_SIZE];
-        if (fgets(buff, sizeof(buff), inf_file) != NULL) {
+        if (fgets(buff, sizeof(buff), inf_file) != NULL)
+        {
             sscanf(buff, "ID:%d", &user_id);
         }
-        if (fgets(buff, sizeof(buff), inf_file) != NULL) {
-            sscanf(buff, "PASSWORD:%s", clients[user_id].password); 
+        if (fgets(buff, sizeof(buff), inf_file) != NULL)
+        {
+            sscanf(buff, "PASSWORD:%s", clients[user_id].password);
         }
 
         strncpy(clients[user_id].username, buffer, BUFFER_SIZE - 1);
@@ -90,4 +104,3 @@ int load_user_name(Client *clients, int max_clients) {
     fclose(file);
     return i;
 }
-
